15b07d6a2cf32c867dcd24daefbaa562e23ff1a0
Bushmakin Mikhail <mbushmakin@vsegda-da.com>
2022-11-02 11:43:30 +0400

    Task* #1 PrimitivesAndWrappers

---
+++ src/main/java/Primitives.java
@@ -0,0 +1,13 @@
+public class Primitives {
+
+    byte a = 127;
+    short b = 32676;
+    int c = 2147483647;
+    long d = 9223372036854775807L;
+    char i = 65535;
+    float f = 85.123456F;
+    double g = 5.123456;
+    boolean h = true;
+
+
+}
---
+++ src/main/java/Wrappers.java
@@ -0,0 +1,13 @@
+public class Wrappers {
+
+    Byte a = 127;
+    Short b = 32676;
+    Integer c = 2147483647;
+    Long d = 9223372036854775807L;
+    Character i = 65535;
+    Float f = 85.123456F;
+    Double g = 5.123456;
+    Boolean h = true;
+
+
+}
